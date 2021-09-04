53c9902adeb2d6325301d5add3c9ffe7f8366b9e
Shane Umayanga <43773386+Shaneumayanga@users.noreply.github.com>
2021-09-05 01:01:50 +0530

    Update database.go
    Clean ups

--- database/database.go
+++ database/database.go
@@ -1,28 +1,17 @@
 package database
 
 import (
+	"github.com/GolangSriLanka/go-puso/config"
 	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-
-	"github.com/GolangSriLanka/go-puso/config"
 )
 
-var db *gorm.DB
-var err error
-
 func Database() *gorm.DB {
-	if db == nil {
-		db, err = gorm.Open(postgres.Open(config.GetEnv("database.URL")), &gorm.Config{})
-
-		if err != nil {
-			log.Panic("failed to connect database")
-		}
-
-		log.Info("database connected")
-
-		return db
+	db, err := gorm.Open(postgres.Open(config.GetEnv("database.URL")), &gorm.Config{})
+	if err != nil {
+		log.Panic("failed to connect database")
 	}
-
+	log.Info("Database connected")
 	return db
 }
